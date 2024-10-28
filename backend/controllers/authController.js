edb950fef7bc551fa28bb3d24747b23ecedac402
Mohit Kumar <mohitguraru@gmail.com>
2024-10-28 23:14:48 +0530

    merge the conflicts

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -3,38 +3,6 @@ import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import User from "../models/userModels.js"; // Adjust the path if necessary
 
-// // Signup Controller
-// export const signup = async (req, res) => {
-//   const { email, name, password, year, regNo, course, profilePicture } =
-//     req.body;
-//   try {
-//     // Check if user already exists
-//     const existingUser = await User.findOne({ email });
-//     if (existingUser) {
-//       return res.status(400).json({ message: "User already exists" });
-//     }
-
-//     // Hash password
-//     const hashedPassword = await bcrypt.hash(password, 12);
-
-//     // Create new user
-//     const newUser = new User({
-//       email,
-//       name,
-//       profilePicture,
-//       password: hashedPassword,
-//       year,
-//       regNo,
-//       course,
-//     });
-
-//     // Save to database
-//     await newUser.save();
-//     res.status(201).json({ message: "User registered successfully" });
-//   } catch (error) {
-//     res.status(500).json({ message: "Something went wrong" });
-//   }
-// };
 // signup controller
 export const signup = async (req, res) => {
   const { email, name, password, year, regNo, course, profilePicture } =
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -9,8 +9,8 @@ const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const error = useSelector((state) => state.auth.error); // Access state
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -18,12 +18,12 @@ const Login = () => {
     try {
       const response = await axios.post("/api/auth/login", { email, password });
       localStorage.setItem("token", response.data.token);
-        console.log("Login successful:", response.data);
-        dispatch(loginSuccess(response.data.token)); // redux state 
-    //   setError("");
+      console.log("Login successful:", response.data);
+      dispatch(loginSuccess(response.data.token)); // redux state 
+      //   setError("");
       navigate("/"); // Redirect to home page
     } catch (err) {
-    dispatch(setError(errorMessage)); // call seterror method
+      dispatch(setError(errorMessage)); // call seterror method
       if (err.response) {
         setError(err.response.data.message || "Invalid email or password");
       } else {
