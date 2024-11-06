7497b6413d84d6981e8b0869ce040b70c3e83c09
Ansh0226 <ansh.114.ar@gmail.com>
2024-11-06 22:57:47 +0530

    Add role state  and fix routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,10 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Navbar from './components/Navbar';
-import Admin from './routes/Admin';
 import Teacher from './routes/Teacher';
 import Student from './routes/Student';
 import Login from './pages/Login';
+import AdminDashboard from './pages/admin/AdminDashboard';
 
 
 function App() {
@@ -17,7 +17,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
 
-        <Route path="/admin/*" element={<Admin />} />
+        <Route path="/admin/*" element={<AdminDashboard />} />
         <Route path="/student/*" element={<Student />} />
         <Route path="/teacher/*" element={<Teacher />} />
      
