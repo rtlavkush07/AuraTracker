a1f304a066a7e5b4de9550e26495dad71dfc607f
rtlavkush07 <rtlavkush07@gmail.com>
2024-11-07 19:00:22 +0530

    changes in teacher dashboard and added addAssignment

--- frontend/src/routes/Teacher.jsx
+++ frontend/src/routes/Teacher.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import TeacherDashboard from '../pages/teacher/TeacherDashboard'
 import TeacherProfile from '../pages/teacher/TeacherProfile'
+import AddAssignment from '../pages/teacher/AddAssignment'
+import AddMaterial from '../pages/teacher/AddMaterial'
 
 const Teacher = () => {
   return (
@@ -9,6 +11,8 @@ const Teacher = () => {
       <Routes>
         <Route path="" element= {<TeacherDashboard />} />
         <Route path="/profile" element  = {<TeacherProfile/>}/>
+        <Route path="/assignment" element  = {<AddAssignment/>}/>
+        <Route path="/addmaterials" element  = {<AddMaterial/>}/>
       </Routes>
     </div>
   )
