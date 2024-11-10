9c058c383be7b2fdb14cd6db9df35eabb2a021bd
Mohit Kumar <mohitguraru@gmail.com>
2024-11-10 07:07:53 +0530

    admin dashboard 4 component UI changed

--- frontend/src/pages/admin/AllStudent.jsx
+++ frontend/src/pages/admin/AllStudent.jsx
@@ -20,16 +20,16 @@ const AllStudent = () => {
   }, []);
 
   return (
-    <div className="flex flex-col items-center p-8  min-h-screen">
-      <h1 className="text-3xl font-bold mb-8 text-gray-800">Students</h1>
-      <ul className="w-full max-w-2xl bg-white rounded-lg shadow-lg px-9">
+    <div className="flex flex-col items-center p-8 min-h-screen overflow-hidden">
+      <h1 className="text-3xl font-bold mb-8 mt-12 text-white">Students</h1>
+      <ul className="w-full max-w-2xl bg-black bg-opacity-40  overflow-scroll rounded-lg shadow-lg px-9">
         {students.map((student, index) => (
           <li key={index} className="border-b border-gray-200 p-4 last:border-none">
-            <p className="text-lg font-semibold text-gray-800">
-              <span className="font-bold text-gray-900">Name:</span> {student.name}
+            <p className="text-lg font-semibold text-white">
+              <span className="font-bold text-black">Name:</span> {student.name}
             </p>
-            <p className="text-lg font-semibold text-gray-800">
-              <span className="font-bold text-gray-900">Email:</span> {student.email}
+            <p className="text-lg font-semibold text-white">
+              <span className="font-bold text-black">Email:</span> {student.email}
             </p>
           </li>
         ))}
